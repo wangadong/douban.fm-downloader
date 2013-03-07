662629ce28d646bc93f479e60491f41975a216bd
Tony Wang <tonywangadong@gmail.com>
2013-03-07 20:19:27 +0800

    Update download.js

--- download.js
+++ download.js
@@ -8,38 +8,31 @@ var failed_retry = 0;
 
 function go()
 {
+    var link_song={};
     try
     {
-        window.DBR.act("skip");     // next song
-        var len = Object.keys(links_json).length;
-        if (last_size === len)
-        {
-            if (failed_retry++ > 10)
-            {
-                console.log("## Stopped ##");
-                return;
-            }
-            else
-            {
-                console.log("## Retrying " + failed_retry + " times ##");
-            }
+        while(current_song_number<songs_numbers){
+            var link=document.getElementById("fm-download-link");
+            link_song[current_song_number]=link;
+            console.log(current_song_number);
+            current_song_number++;
+            window.DBR.act("skip");     // next song
+            var len = Object.keys(link_song).length;
+              if ( len % 10 === 0)
+                {
+                    download_json();
+                }
+                var sleep_time = Math.ceil(Math.random() * (6 - 4) + 4);
         }
-
-        // failed_retry = 0;
-        console.log("Current list size: " + len);
-        last_size = len;
-        if (len % 10 === 0)
-        {
-            download_json();
-        }
-
-        var sleep_time = Math.ceil(Math.random() * (6 - 4) + 4);
-        download_timeout = setTimeout(go, sleep_time * 1000);
+        
+        
+     
+       
     }
     catch (err)
     {
         console.error(err);
-        download_timeout = setTimeout(go, 60 * 1000);
+      
     }
 }
 
@@ -50,23 +43,7 @@ function stop()
 
 function download_json()
 {
-    $("<a href='" + "data:application/x-json;base64," + encodeURIComponent(JSON.stringify(links_json)) + "' download='list.json'/>")[0].click();
-}
-
-
-// Thanks to `douban.fm Hacker`
-// hook to douban.fm's handler
-var extStatusHandlerBak = window.extStatusHandler;
-var links_json = {};
-window.extStatusHandler = function(a) {
-    extStatusHandlerBak(a);
-    var o = eval('(' + a + ')');
-    if (o.type == 'start') {
-        var s = o.song;
-        var name = s.title + ".mp3";
-        links_json[s.url] = name;
-        // links_json[name] = s.url;
-    }
+    $("<a href='" + "data:application/x-json;base64," + encodeURIComponent(JSON.stringify(link_song)) + "' download='list.json'/>")[0].click();
 }
 
 go();
