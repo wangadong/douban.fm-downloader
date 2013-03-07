9ae710ec1c04155fc780b7e448c2d0950fc4f835
Tony Wang <tonywangadong@gmail.com>
2013-03-07 20:24:52 +0800

    Update download.js

--- download.js
+++ download.js
@@ -5,12 +5,13 @@ var download_timeout, current_song_number = 0;
 var songs_number = 500;
 var last_size = -1;
 var failed_retry = 0;
-
+ var link_song={};
 function go()
 {
-    var link_song={};
+   
 
         while(current_song_number<songs_number){
+            
             var link=document.getElementById("fm-download-link");
             link_song[current_song_number]=link;
             console.log(current_song_number);
