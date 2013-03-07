4d8c0babd9096daf0b4b4a2404a782b762fbb2b1
Tony Wang <tonywangadong@gmail.com>
2013-03-07 21:40:13 +0800

    Update download.js

--- download.js
+++ download.js
@@ -1,9 +1,6 @@
 // Author: Hua Liang[Stupid ET]
 // Website: http://EverET.org
-// var script = document.createElement('script');
-// script.src = 'http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js';
-// script.type = 'text/javascript';
-// document.getElementsByTagName('head')[0].appendChild(script);
+
 var current_song_number = 0;
 var songs_number = 500;
 var link_song;
@@ -17,7 +14,8 @@ function go()
 //            if(link_song.indexOf(link)==-1)
             link_song=link_song+'<br>'+link;
             var popup = window.open("about:blank","Popup");
-            popup.document.write(link_song);
+            //popup.document.write(link_song);
+            popup.window.open(link);
          
             console.log(current_song_number);
             current_song_number++;            
