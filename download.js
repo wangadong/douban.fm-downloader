c99ed12c40488d10eb090121b651ffe5b53404a1
Tony Wang <tonywangadong@gmail.com>
2013-03-07 20:29:15 +0800

    Update download.js

--- download.js
+++ download.js
@@ -1,11 +1,9 @@
 // Author: Hua Liang[Stupid ET]
 // Website: http://EverET.org
 
-var download_timeout, current_song_number = 0;
+var current_song_number = 0;
 var songs_number = 500;
-var last_size = -1;
-var failed_retry = 0;
- var link_song={};
+var link_song;
 function go()
 {
    
@@ -13,15 +11,10 @@ function go()
         while(current_song_number<songs_number){
             
             var link=document.getElementById("fm-download-link");
-            link_song[current_song_number]=link;
+            link_song=link_song+'/n'+link;
             console.log(current_song_number);
             current_song_number++;
             window.DBR.act("skip");     // next song
-            var len = Object.keys(link_song).length;
-              if ( len % 10 === 0)
-                {
-                    download_json();
-                }
                 var sleep_time = Math.ceil(Math.random() * (6 - 4) + 4);
         }
         
@@ -30,9 +23,5 @@ function go()
   
 }
 
-function download_json()
-{
-    $("<a href='" + "data:application/x-json;base64," + encodeURIComponent(JSON.stringify(link_song)) + "' download='list.json'/>")[0].click();
-}
 
 go();
