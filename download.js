a4d72f0c8ed96f7555f772474d793f7a9c5d7ab7
Tony Wang <tonywangadong@gmail.com>
2013-03-07 21:34:01 +0800

    Update download.js

--- download.js
+++ download.js
@@ -1,41 +1,29 @@
 // Author: Hua Liang[Stupid ET]
 // Website: http://EverET.org
-var script = document.createElement('script');
-script.src = 'http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
+// var script = document.createElement('script');
+// script.src = 'http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js';
+// script.type = 'text/javascript';
+// document.getElementsByTagName('head')[0].appendChild(script);
 var current_song_number = 0;
 var songs_number = 500;
 var link_song;
-var link;
+
 function go()
 {
    
             window.DBR.act("skip");     // next song
                 
-            link=document.getElementById("fm-download-link");
+            var link=document.getElementById("fm-download-link");
 //            if(link_song.indexOf(link)==-1)
             link_song=link_song+'<br>'+link;
-            open_in_new_tab(link);
+            var popup = window.open("about:blank","Popup");
+            popup.document.write(link_song);
+         
             console.log(current_song_number);
             current_song_number++;            
-            var sleep_time = Math.ceil(Math.random() * (6 - 4) + 4);
-            download_timeout = setTimeout(go, sleep_time * 10000);
+            download_timeout = setTimeout(go, 10000);
         
 }
-function open_in_new_tab(url)
-{
- $("a.runReport").click(function(evt) {
-    // open a popup within the click handler
-    // this should open in a new tab
-    var popup = window.open("about:blank","Popup");
 
-    //do some ajax calls
-    
-        popup.document.write(link_song);
-
-     
-});
-}
 
 go();
