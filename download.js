79ed230efb50b42107ef74156bcb8619cf9910d6
Tony Wang <tonywangadong@gmail.com>
2013-04-04 20:11:38 +0800

    Update download.js

--- download.js
+++ download.js
@@ -5,23 +5,24 @@ var current_song_number = 0;
 var songs_number = 500;
 var link_song=new Array();
 var timeout=1000;
-var link
+var link;
+var link_url;
 function go()
 {
            download_timeout = setTimeout(go, timeout);
                                   // next song
 
            link=document.getElementById("fm-download-link");
-           console.log(link);
-
+           link_url=link+"";
+           if(contains(link_url)){
             var popup = window.open("about:blank","Popup");
             //popup.document.write(link_song);
             popup.window.open(link,"_self");
          
            console.log(current_song_number);
               
-            link_song.push(link+"");
-      
+            link_song.push(link_url);
+           }
 
             window.DBR.act("skip"); 
          current_song_number=current_song_number+1;      
