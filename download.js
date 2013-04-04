1c046f97e57081da6297ee611b9c9442f44bb387
Tony Wang <tonywangadong@gmail.com>
2013-04-04 18:43:42 +0800

    Update download.js

--- download.js
+++ download.js
@@ -3,28 +3,47 @@
 
 var current_song_number = 0;
 var songs_number = 500;
-var link_song;
+var link_song=new Array();
 var timeout=1000;
 function go()
 {
-   
+           download_timeout = setTimeout(go, timeout);
             window.DBR.act("skip");     // next song
 
             var link=document.getElementById("fm-download-link");
+            var i=current_song_number;
+           if(contains(link)){
+               window.DBR.act("skip");     // next song
+               go();
+           }
+           else{
 
-//            if(link_song.indexOf(link)==-1)
-            link_song=link_song+'<br>'+link;
             var popup = window.open("about:blank","Popup");
             //popup.document.write(link_song);
             popup.window.open(link,"_self");
          
             console.log(current_song_number);
+            link_song[current_song_number]=link;
             current_song_number++;            
-            download_timeout = setTimeout(go, timeout);
+            
+           }
             if(current_song_number>songs_number)
               stop();
 }
 
+ //JS判断某一项是否在数组中
+ function contains(str)
+    {
+
+      for(var i = 0;i < link_song.length;i++)
+      {
+        if(array[i] == str)
+        {
+         return true;
+        }
+      }
+      return false;
+    }
 function stop()
 {
     clearTimeout(download_timeout);
